b15ec6ab2c92d89cc9f52e128d126ce814a84e0c
Jack Cole <jack@jack-cole.com>
2016-10-28 16:32:45 -0700

    Testing creating templates
    Added some test templates

--- Web/front_end/front_end/urls.py
+++ Web/front_end/front_end/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^test$', TemplateView.as_view(template_name='test.html')),
+    url(r'^submit$', TemplateView.as_view(template_name='forms/submit.html')),
+    url(r'^samplesurvery$', TemplateView.as_view(template_name='forms/sample_survery.html')),
 ]
